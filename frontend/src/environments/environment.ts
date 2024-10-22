28248ae895d04f22315202d497f2b107f318f1e2
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-10-22 05:01:51 -0400

    frontend working

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-i6mkw7r670gmhfzt.us', // the auth0 domain prefix
-    audience: 'https://jcscoffeeshop510699.com/auth/api', // the audience set for the auth0 app
-    clientId: 'cBBIAcAUcp3mmqa5mvurevl1ARfl8FIx', // the client id generated for the auth0 app
+    url: 'dev-8his2amisscpohz8.us', // the auth0 domain prefix
+    audience: 'https://jcscoffeeshop510698.com', // the audience set for the auth0 app
+    clientId: 'P0mR4aWlAZg4wCMVpudRTmrr3CGQcYZM', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
